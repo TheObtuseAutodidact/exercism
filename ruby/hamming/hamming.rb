f9d87d79040886b4fe570aa598a76198c96801fe
Obtuse <theobtuseautodidact@gmail.com>
2016-10-15 22:20:34 -0700

    Refactor hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,14 +1,8 @@
 class Hamming
   VERSION = 1
   def self.compute(strand_1, strand_2)
-    if strand_1.length != strand_2.length
-      raise ArgumentError
-    else
-      sol = 0
-      strand_1.split("").each_with_index do | n, i |
-        sol += 1 if n != strand_2.split("")[i]
-      end
-      sol
-    end
+    raise ArgumentError,
+      "Strands must be of equal length" if strand_1.length != strand_2.length
+    strand_1.chars.zip(strand_2.chars).select { |k, v| k != v }.length
   end
 end
