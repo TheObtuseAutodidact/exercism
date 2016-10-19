b699ddfdd8881d15dfa43f003149a36cc73a42af
Obtuse <theobtuseautodidact@gmail.com>
2016-10-18 22:06:28 -0700

    Python - Bob - first submission

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,6 +1,10 @@
-#
-# Skeleton file for the Python "Bob" exercise.
-#
 def hey(what):
-
-    return
+    what = what.strip()
+    if not what:
+        return 'Fine. Be that way!'
+    elif what.upper() == what and what.upper() != what.lower():
+        return 'Whoa, chill out!'
+    elif what[-1] == '?':
+        return "Sure."
+    else:
+        return 'Whatever.'
