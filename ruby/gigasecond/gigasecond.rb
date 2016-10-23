179ecb466220e0b66073d872eaef426e9dd33618
Obtuse <theobtuseautodidact@gmail.com>
2016-10-22 20:43:57 -0700

    Ruby - Gigasecond solution 2 cleaned up

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,8 +1,6 @@
 class Gigasecond
   GIGASECOND = 1e9
   def self.from(time)
-    # x = time.class
-    # puts x.class
     raise TypeError, ".from accepts Time objects" unless time.class == Time
     time + GIGASECOND
   end
