f55933eb09f90d8a24e36dd122035b89c410e70f
Obtuse <theobtuseautodidact@gmail.com>
2017-01-08 07:24:58 -0800

    Second solution to to Gigasecond

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -4,12 +4,9 @@ import "time"
 
 const (
 	testVersion = 4
+	gigasecond  = time.Duration(time.Second * 1000000000)
 )
 
-var second = time.Second
-
 func AddGigasecond(t time.Time) time.Time {
-	gigasecond := second * 1000000000 // one billion
-	duration := time.Duration(gigasecond)
-	return t.Add(duration)
+	return t.Add(gigasecond)
 }
