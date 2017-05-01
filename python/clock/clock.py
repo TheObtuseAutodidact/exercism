02bf1b96a7a42fb013abc78c54b08bd26ceab416
Obtuse <theobtuseautodidact@gmail.com>
2017-04-30 23:13:29 -0700

    first solution for clock python

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -2,36 +2,21 @@
 class Clock:
 
     def __init__(self, hour, minute):
-        self.hour = format(hour, '02d')
-        self.minute = format(minute, '02d')
+        self.minute = ((hour * 60 ) + minute) % (60 * 24)
+
+
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.minute == other.minute
+
 
     def __str__(self):
-        return "{}:{}".format(self.hour, self.minute)
+        display_hour = format(((self.minute // 60) % 24 ), '02d')
+        display_minute = format((self.minute % 60), '02d')
+        return "{}:{}".format(display_hour, display_minute)
+
 
     def add(self, num):
-        # hours = num / 60
-        # minutes = num % 60
-        # self.minute += minutes
-        # self.hour += hours
-        # self.hour %= 24
-        # return self
-        advance_minute(self.minute, num)
-        self.hour += minutes_to_hours(self.minute, num)
-        advance_hour(self.hour, num)
+        self.minute += num
         return self
-
-    def advance_hour(self, h_sum, addend):
-        h_sum += addend
-        h_sum %= 24
-        # return  h_sum
-
-    def advance_minute(self, m_sum, addend):
-        m_sum += addend
-        import pdb; pdb.set_trace()
-        m_sum %= 60
-        # return minutes
-
-    def minutes_to_hours(self, m_sum, addend):
-        m_sum += addend
-        hours = m_sum / 60
-        return hours
