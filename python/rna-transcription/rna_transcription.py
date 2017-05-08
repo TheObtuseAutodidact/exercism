2928bbf0a88870cd01911f4dd94376b0ee45baf9
Obtuse <theobtuseautodidact@gmail.com>
2017-05-07 22:27:18 -0700

    Third solution to rna-transcription python
    minor performance enhancement

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,14 +1,10 @@
-# * `G` -> `C`
-# * `C` -> `G`
-# * `T` -> `A`
-# * `A` -> `U`
 DNA_TO_RNA = { "G": "C", "C": "G", "T": "A", "A": "U" }
 
 def to_rna(dna_string):
-    rna_string = ""
+    rna_string = []
     for each in dna_string:
         if each not in DNA_TO_RNA:
             return ""
         else:
-            rna_string += DNA_TO_RNA[each]
-    return rna_string
+            rna_string.append(DNA_TO_RNA[each])
+    return "".join(rna_string)
