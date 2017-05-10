e0620896fba19f4cf25e6414f0c9fda2e97a1077
Obtuse <theobtuseautodidact@gmail.com>
2017-05-09 23:32:29 -0700

    hamming third submission python

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,4 +2,4 @@ def distance(string1, string2):
     if len(string1) != len(string2):
         raise ValueError
     pairs = list(zip(string1, string2))
-    return [True for pair in pairs if pair[0] != pair[1]].count(True)
+    return len([True for pair in pairs if pair[0] != pair[1]])
