d92099bfd3f100638a4586cf709622cad42afb7e
Obtuse <theobtuseautodidact@gmail.com>
2017-05-25 22:39:25 -0700

    Second solution gigasecond javascript

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,10 +1,9 @@
-var Gigasecond = function(input){
+var Gigasecond = function(input) {
     this.d = input;
 };
 
 Gigasecond.prototype.date = function() {
-    var gs = new Date(this.d);
-    return new Date(gs.setUTCSeconds(this.d.getUTCSeconds() + Math.pow(10,9)))
+    return new Date(new Date(this.d).setUTCSeconds(this.d.getUTCSeconds() + Math.pow(10, 9)))
 };
 
 module.exports = Gigasecond;
