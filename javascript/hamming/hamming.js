8a0bc16c7992733587755ced8ddf0d7736966acc
Obtuse <theobtuseautodidact@gmail.com>
2017-06-13 22:05:36 -0700

    Clean up second attempt at hamming javascript

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -4,13 +4,7 @@ Hamming.prototype.compute = function(string1, string2) {
     if (string1.length !== string2.length)
         throw "DNA strands must be of equal length.";
     var count = 0
-        // for (i=0; i < string1.length; i++) {
-        //     if (string1[i] !== string2[i])
-        //         count++
-        // }
-        // return count
     string1.split("").forEach(function(char, idx) {
-        // console.log(arr)
         if (char !== string2[idx]) {
             count++
         }
