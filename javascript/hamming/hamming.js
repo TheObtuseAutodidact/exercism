1b94ac6fd9b6d1393b425589fa41798763a33502
Obtuse <theobtuseautodidact@gmail.com>
2017-06-13 21:58:19 -0700

    Second solution to hamming javascript

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -3,11 +3,18 @@ var Hamming = function() {}
 Hamming.prototype.compute = function(string1, string2) {
     if (string1.length !== string2.length)
         throw "DNA strands must be of equal length.";
-    count = 0
-    for (i=0; i < string1.length; i++) {
-        if (string1[i] !== string2[i])
+    var count = 0
+        // for (i=0; i < string1.length; i++) {
+        //     if (string1[i] !== string2[i])
+        //         count++
+        // }
+        // return count
+    string1.split("").forEach(function(char, idx) {
+        // console.log(arr)
+        if (char !== string2[idx]) {
             count++
-    }
+        }
+    })
     return count
 }
 
