54e7ae09b6d25feb9ae666a579b9d27ac0ac7957
Obtuse <theobtuseautodidact@gmail.com>
2017-06-14 22:59:13 -0700

    Third solution hamming javascript

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -3,13 +3,9 @@ var Hamming = function() {}
 Hamming.prototype.compute = function(string1, string2) {
     if (string1.length !== string2.length)
         throw "DNA strands must be of equal length.";
-    var count = 0
-    string1.split("").forEach(function(char, idx) {
-        if (char !== string2[idx]) {
-            count++
-        }
-    })
-    return count
+    return string1.split("").filter(function(char, idx) {
+        return char !== string2[idx]
+    }).length
 }
 
 module.exports = Hamming;
