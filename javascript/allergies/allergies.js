31f97fa6a91d70db506b5b497dbde4ab31493111
Obtuse <theobtuseautodidact@gmail.com>
2017-06-25 20:38:37 -0700

    Second solution allergies javascript

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -1,3 +1,5 @@
+allergin_list = ["eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"]
+
 var Allergies = function(num) {
     this.num = num;
 };
@@ -7,37 +9,12 @@ Allergies.prototype.list = function() {
         this.num -= 256
     }
     var solution_array = []
-    if (this.num - 128 >= 0) {
-        solution_array.unshift("cats")
-        this.num -= 128;
-    }
-    if (this.num - 64 >= 0) {
-        solution_array.unshift("pollen")
-        this.num -= 64;
-    }
-    if (this.num - 32 >= 0) {
-        solution_array.unshift("chocolate")
-        this.num -= 32;
-    }
-    if (this.num - 16 >= 0) {
-        solution_array.unshift("tomatoes")
-        this.num -= 16;
-    }
-    if (this.num - 8 >= 0) {
-        solution_array.unshift("strawberries")
-        this.num -= 8;
-    }
-    if (this.num - 4 >= 0) {
-        solution_array.unshift("shellfish")
-        this.num -= 4;
-    }
-    if (this.num - 2 >= 0) {
-        solution_array.unshift("peanuts")
-        this.num -= 2;
-    }
-    if (this.num % 2 === 1) {
-        solution_array.unshift("eggs")
-        this.num -= 1;
+
+    for (var i = 7; i >= 0; i--) {
+        if (Math.pow(2, i) <= this.num) {
+            solution_array.unshift(allergin_list[i])
+            this.num -= Math.pow(2, i)
+        }
     }
     return solution_array
 }
