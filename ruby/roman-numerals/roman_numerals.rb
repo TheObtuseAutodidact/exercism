6200bf2a29c7bdd24a8235ee4b737142f15a24e5
Obtuse <theobtuseautodidact@gmail.com>
2017-09-03 12:57:02 -0700

    First solution roman-numerals ruby

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,18 +1,15 @@
 module RomanNumerals
+
   {'1': 'I', '4': 'IV', '5': 'V', '9': 'IX', '10': 'X'}
   def to_roman
     thousands = self / 1000
-    five_hundreds = (self % 1000) / 500
-    hundreds = (self % 500) / 100
-    fifties = (self % 100) / 50
-    tens = (self % 50) / 10
-    # fives = (self % 10) / 5
+    hundreds = (self % 1000) / 100
+    tens = (self % 100) / 10
     ones =  (self % 10)
+    
     string = ""
     string += construct_thousands(thousands)
-    string += construct_five_hundreds(five_hundreds)
     string += construct_hundreds(hundreds)
-    stirng += construct_fifties(fifties)
     string += construct_tens(tens)
     string += construct_ones(ones)
 
@@ -22,14 +19,6 @@ module RomanNumerals
     def construct_thousands(thousands)
       if thousands > 0
           "M" * thousands
-      # else
-      #   ""
-      end
-    end
-
-    def construct_five_hundreds(five_hundreds)
-      if five_hundreds > 0
-        'D' * five_hundreds
       else
         ""
       end
@@ -39,10 +28,10 @@ module RomanNumerals
       if hundreds > 0
         if hundreds == 9
           'CM'
-        elsif (6..8).include?(hundreds)
+        elsif (5..8).include?(hundreds)
           hundreds -= 5
           'D' + ('C' * hundreds)
-        elsif hundreds = 4
+        elsif hundreds == 4
           'CD'
         elsif (1..3).include?(hundreds)
           'C' * hundreds
@@ -52,28 +41,19 @@ module RomanNumerals
       end
     end
 
-    def construct_fifties(fifties)
-      puts fifties
-      if fifties > 0
-        'L'
-      else
-        ""
-      end
-    end
-
     def construct_tens(tens)
       if tens > 0
         if tens == 9
           'XC'
-        elsif (6..8).include?(tens)
-          'L' + ('X' * tens)
+        elsif (5..8).include?(tens)
+          'L' + ('X' * (tens - 5))
         elsif tens == 4
           'XL'
         elsif (1..3).include?(tens)
           'X' * tens
-        else
-          ""
         end
+      else
+        ""
       end
     end
 
@@ -81,7 +61,7 @@ module RomanNumerals
       if ones == 9
         'IX'
       elsif (6..8).include?(ones)
-        'V' + ('I' * ones)
+        'V' + ('I' * (ones - 5))
       elsif ones == 5
         'V'
       elsif ones == 4
@@ -99,4 +79,8 @@ end
 
 class Integer
   include RomanNumerals
+end
+
+module BookKeeping
+  VERSION = 2
 end
