a50a63a64cfc3d636948e9a844e8a22ec3127e5b
Obtuse <theobtuseautodidact@gmail.com>
2017-09-09 22:08:43 -0700

    Second solution grains ruby

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,16 +1,21 @@
 class Grains
 
-    def self.square(num)
-        raise ArgumentError.new("Out of Range") if num > 64 || num < 1
-        2**(num-1)
-    end
+  SQUARE_MAX = 64
+  SQUARE_MIN = 1
 
-    def self.total
-        (1..64).reduce { |sum, num| sum += square(num) }
-    end
+  def self.square(num)
+    if !(SQUARE_MIN..SQUARE_MAX).include?(num)
+      raise ArgumentError.new("Out of Range: Valid range [1 to 64] inclusive")
+    end 
+    2**(num-1)
+  end
+
+  def self.total
+    (SQUARE_MIN..SQUARE_MAX).reduce { |sum, num| sum += square(num) }
+  end
 
 end
 
 module BookKeeping
-    VERSION = 1
+  VERSION = 1
 end
