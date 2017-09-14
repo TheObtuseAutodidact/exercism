56d9a6a728f2bbcb43d869eceb791c3d2a0cca2c
Obtuse <theobtuseautodidact@gmail.com>
2017-09-13 21:50:54 -0700

    Fourth solution grains ruby

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,31 +1,37 @@
 class Grains
 
-  VALID_SQUARE_RANGE = (1..64)
+  VALID_SQUARE_RANGE = (1..64) # Squares
 
 
   def self.square(num)
     fail_if_out_of_range(num)
-    2**(num-1)
+    2**(num-1) # Minus 1 for zero-indexing
   end
 
   def self.total
     (VALID_SQUARE_RANGE).reduce { |sum, num| sum += square(num) }
   end
 
-  def self.fail_if_out_of_range(num)
-    unless (VALID_SQUARE_RANGE).include?(num)
-      raise OffBoardError.new(
-        "Out of Range:" +
-        "Valid range [#{VALID_SQUARE_RANGE.min} to " +
-        "#{VALID_SQUARE_RANGE.max}] inclusive"
-      )
+  private
+    def self.fail_if_out_of_range(num)
+      unless (VALID_SQUARE_RANGE).include?(num)
+        raise OffBoardError.new(VALID_SQUARE_RANGE, num)
+      end
     end
-  end
 
 end
 
 class OffBoardError < ArgumentError
+  def initialize(board_range, num)
+    @board_range = board_range
+    @num = num
+  end
+
+  def message
+    "#{@num} Out of Range: Valid range #{@board_range} (inclusive)"
+  end
 end
+
 
 module BookKeeping
   VERSION = 1
