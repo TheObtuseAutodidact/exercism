318a2496f64b655f522440fc0476a9e34d988ffc
Obtuse <theobtuseautodidact@gmail.com>
2017-09-14 23:29:51 -0700

    Second solution word-count ruby

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,22 +1,38 @@
 class Phrase
-    def initialize(words)
-        @words = words
+  def initialize(words)
+    @words = words
+  end
+
+  def word_count
+    words = split_and_sanitize_to_word_list(@words)
+    words.each_with_object(Hash.new(0)) { |word, my_hash| my_hash[word] += 1 }
+  end
+
+  private 
+    def strip_quotes(str)
+      if (str.start_with?("\"") && str.end_with?("\"")) || 
+        (str.start_with?("\'") && str.end_with?("\'"))
+        str = str[1..-2]
+      end
+      str
     end
 
-    def word_count
-        word_count_hash = Hash.new(0)
-        words = @words.gsub(/[^[:alnum:]']/, " ")
-        words.split().each do |word|
-            if word[0] == word[-1] &&
-                word[0] == "\"" || word[0] == "\'"
-                word = word[1..-2]
-            end
-            word_count_hash[word.downcase] += 1
-        end
-        word_count_hash
+    def split_word_list(str)
+      str.gsub(/[^[:alnum:]']/, " ")
     end
+
+    def sanitize_word_list(str)
+      str.split().collect { |word| strip_quotes(word).downcase }
+    end
+
+    def split_and_sanitize_to_word_list(str)
+      sanitize_word_list(split_word_list(str))
+    end
+
+
+
 end
 
 module BookKeeping
-    VERSION = 1
+  VERSION = 1
 end
