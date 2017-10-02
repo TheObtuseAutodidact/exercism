be78e8c8bdabe833cdb818e34e78518844e9a2d7
Obtuse <theobtuseautodidact@gmail.com>
2017-10-01 23:14:04 -0700

    Third solution run-length-encoding ruby

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,3 +1,5 @@
+# a simple form of data compression, where runs
+# (consecutive data elements) are replaced by just one data value and count
 class RunLengthEncoding
   def self.encode(str)
     str.chars.chunk_while { |a, b| a == b }.map(&:join).collect do |subset|
@@ -6,21 +8,9 @@ class RunLengthEncoding
   end
 
   def self.decode(str)
-    decoded_str = ''
-    number_of_letters = ''
-    str.length.times do |i|
-      if str[i].to_i.to_s == str[i]
-        number_of_letters += str[i]
-      else
-        if number_of_letters.empty?
-          decoded_str += str[i]
-        else
-          decoded_str += str[i] * number_of_letters.to_i
-          number_of_letters = ''
-        end
-      end
-    end
-    decoded_str
+    str.scan(/.*?[a-zA-Z ]/).collect do |char|
+      char.length == 1 ? char : char[-1] * char[0..-2].to_i
+    end.join
   end
 end
 
