4bcc283670734ca617d2a0ae0fe8edc0674c7ebb
Obtuse <theobtuseautodidact@gmail.com>
2017-10-02 21:15:11 -0700

    Fourth solution run_length_encoding ruby

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -2,15 +2,16 @@
 # (consecutive data elements) are replaced by just one data value and count
 class RunLengthEncoding
   def self.encode(str)
-    str.chars.chunk_while { |a, b| a == b }.map(&:join).collect do |subset|
-      subset.length > 1 ? subset.length.to_s + subset.chr : subset.chr
-    end.join
+    str.chars.chunk_while { |a, b| a == b }
+    .map(&:join)
+    .collect { |str| str.length > 1 ? str.length.to_s + str.chr : str.chr }
+    .join
   end
 
   def self.decode(str)
-    str.scan(/.*?[a-zA-Z ]/).collect do |char|
-      char.length == 1 ? char : char[-1] * char[0..-2].to_i
-    end.join
+    str.scan(/.*?[a-zA-Z ]/)
+    .collect { |char| char.length == 1 ? char : char[-1] * char[0..-2].to_i }
+    .join
   end
 end
 
