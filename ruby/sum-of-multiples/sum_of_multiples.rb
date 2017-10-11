8fca0411235469f8106c8782a7347705bf7ac8ac
Obtuse <theobtuseautodidact@gmail.com>
2017-10-10 17:09:05 -0700

    First solution sum_of_multiples ruby

---
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -0,0 +1,19 @@
+class SumOfMultiples
+  def initialize(*multiples)
+    @multiples = multiples
+  end
+
+  def to(limit)
+    sum = []
+    (1...limit).each do |num|
+      @multiples.each do |multiple|
+        sum << num if (num % multiple).is_zero?
+      end
+    end
+    sum.uniq.sum
+  end
+end
+
+module BookKeeping
+  VERSION = 1
+end
